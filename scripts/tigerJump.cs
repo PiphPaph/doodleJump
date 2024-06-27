5c21f6a5e5300d969fff2bfac5a83ab59eecef14
PiphPaph <freekill1488@mail.ru>
2024-06-27 15:54:22 +0300

    fix colliders for platforms

--- scripts/tigerJump.cs
+++ scripts/tigerJump.cs
@@ -2,13 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class tigerJump : MonoBehaviour
 {
+    private bool onPlatform;
+    private GameObject Tiger;
+
+    void Start() 
+    {
+        Tiger = GameObject.Find("tigerTheJumper");
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "tiger")
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 15f, ForceMode2D.Impulse);
+            onPlatform = true;
+            Debug.Log(onPlatform);
         }
+    }
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "tiger")
+        {
+            onPlatform = false;
+            Debug.Log(onPlatform);
+        }
+    }
+    
+    void Update() 
+    {
+        if (onPlatform == true) 
+        {
+           Tiger.GetComponent<Rigidbody2D>().AddForce(transform.up * 15f, ForceMode2D.Impulse);
+        }
+        
     }
 }
